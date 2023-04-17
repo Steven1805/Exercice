bd6bf807368e4a6240600760b14a4b124e0c57c6
Steven1805 <hei.steven.2@gmail.com>
2023-04-17 15:47:32 +0300

    fix: add componentScan

--- src/main/java/com/example/exercice/ExerciceApplication.java
+++ src/main/java/com/example/exercice/ExerciceApplication.java
@@ -2,8 +2,9 @@ package com.example.exercice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@ComponentScan("com.example.exercice")
 public class ExerciceApplication {
 
     public static void main(String[] args) {
