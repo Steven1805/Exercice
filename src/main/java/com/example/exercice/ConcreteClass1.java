c640a3e13e1f3c5d41733a28f76f6940e99d668c
Steven1805 <hei.steven.2@gmail.com>
2023-04-17 13:51:54 +0300

    feat: add conctrete classes

---
+++ src/main/java/com/example/exercice/ConcreteClass1.java
@@ -0,0 +1,4 @@
+package com.example.exercice;
+
+public class ConcreteClass1 extends AbstractClass1{
+}
---
+++ src/main/java/com/example/exercice/ConcreteClass2.java
@@ -0,0 +1,4 @@
+package com.example.exercice;
+
+public class ConcreteClass2 extends AbstractClass1{
+}
